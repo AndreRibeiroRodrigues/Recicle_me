b9bb82701927e2957cab2a3160026bd545716baa
games.andre77 <games.andre77@gmail.com>
2025-06-18 20:38:02 +0000

    Firebabase Configurado_18/061737

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:recicle_me/firebase_options.dart';
 import 'package:recicle_me/scr/myApp.dart';
 
-<<<<<<< HEAD
-void main() {
-  
-=======
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
->>>>>>> e7f66e3 (Firebase Configurado_18061713)
   runApp(const MyApp());
 }
 
--- lib/scr/myApp.dart
+++ lib/scr/myApp.dart
@@ -8,11 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
-      home: Homepage(),
-=======
       home: const Homepage(),
->>>>>>> e7f66e3 (Firebase Configurado_18061713)
     );
   }
 }
